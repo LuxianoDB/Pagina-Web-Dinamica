a4e00b23674392c5ce886556d03d6dd61a038861
Luciano Di Battista <134889518+LuxianoDB@users.noreply.github.com>
2024-04-18 17:12:41 -0300

    js/

---
+++ 1C Javascript DOM - Mensaje Secreto - getElementById/funciones.js
@@ -0,0 +1,19 @@
+function funcion(){
+    let contenido = document.getElementById("inputtext").value;
+    alert(reverseParentheses(contenido));
+}
+
+function reverseParentheses(s){
+    let j = s;
+    if (s.includes('(')){
+        j = reverseParentheses(reverseOnce(s));
+    }   
+    return j;
+}
+
+function reverseOnce(s){
+    var regexp = /\(([^()]*)\)/i;
+    var subStr = regexp.exec(s)[1];
+    subStr = subStr.split('').reverse().join('');
+    return s.replace(regexp, subStr)
+}
